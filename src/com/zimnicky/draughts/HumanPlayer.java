a335383d0df4d6f1f3461d76d7a00deacaa098ae
Pawel Zimnicki <zimnicky@gmail.com>
2014-11-12 22:38:37 +0200

    + Human Player

---
+++ src/com/zimnicky/draughts/HumanPlayer.java
@@ -0,0 +1,60 @@
+package com.zimnicky.draughts;
+
+
+public class HumanPlayer extends Player {
+
+    private boolean waitingMoveStatus;
+    private Game.Move move;
+    private Game.Move previousMove;
+    private Game game;
+
+    synchronized private void waitMove() {
+        waitingMoveStatus = true;
+        try {
+            wait();
+        }catch (InterruptedException e){
+            assert false;
+        }
+    }
+
+    public Game.Move getPreviousMove() {
+        return previousMove;
+    }
+
+    public HumanPlayer() {
+        waitingMoveStatus = false;
+    }
+
+    public Game.Move makeMove(Game game, Game.Move lastMove, boolean beatSequence)
+    {
+        this.game = game;
+        move = null;
+        if (beatSequence) {
+            previousMove = lastMove;
+        }
+        else {
+            previousMove = null;
+        }
+        while (!game.canMove(move, previousMove)) {
+            waitMove();
+        }
+        return move;
+    }
+
+    public boolean canMove(Game.Move move){
+        return game.canMove(move, previousMove);
+    }
+
+    synchronized public boolean isWaitMove() {
+        return waitingMoveStatus;
+    }
+
+    synchronized public void setMove(Game.Move move) {
+        if (!waitingMoveStatus){
+            return;
+        }
+        this.move = new Game.Move(move);
+        waitingMoveStatus = false;
+        notify();
+    }
+}
